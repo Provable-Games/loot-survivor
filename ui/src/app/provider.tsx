0c3735f45215563d7bfa8178c35a3ace5958b43c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-06-01 21:17:23 +1000

    init controller

--- ui/src/app/provider.tsx
+++ ui/src/app/provider.tsx
@@ -5,8 +5,65 @@ import {
   StarknetConfig,
   alchemyProvider,
   blastProvider,
+  starkscan,
+  jsonRpcProvider,
 } from "@starknet-react/core";
 import { goerli, mainnet, sepolia } from "@starknet-react/chains";
+import { Chain } from "@starknet-react/chains";
+import CartridgeConnector from "@cartridge/connector";
+
+function rpc(_chain: Chain) {
+  return {
+    nodeUrl: process.env.NEXT_PUBLIC_RPC_URL,
+  };
+}
+
+const cartridgeConnectors = [
+  new CartridgeConnector(
+    [
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "new_game",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "explore",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "attack",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "flee",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "equip",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "drop",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_GAME_ADDRESS!,
+        method: "upgrade",
+      },
+      {
+        target: process.env.NEXT_PUBLIC_LORDS_ADDRESS!,
+        method: "approve",
+      },
+    ],
+    {
+      theme: {
+        colors: {
+          primary: "#0ad3ff",
+          secondary: "#78ffd6",
+        },
+      },
+    }
+  ) as never as Connector,
+];
 
 export function StarknetProvider({
   connectors,
@@ -24,10 +81,11 @@ export function StarknetProvider({
   const chains = onMainnet ? [mainnet] : onSepolia ? [sepolia] : [goerli];
   return (
     <StarknetConfig
-      connectors={connectors}
       autoConnect
-      provider={provider}
-      chains={chains}
+      chains={[sepolia]}
+      connectors={cartridgeConnectors}
+      explorer={starkscan}
+      provider={jsonRpcProvider({ rpc })}
     >
       {children}
     </StarknetConfig>
