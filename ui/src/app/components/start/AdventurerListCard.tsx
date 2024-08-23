e953a291bc8df9d94613fee1b601932e6dd9f8da
Starknet Dev <starknetdev@gmail.com>
2024-08-23 14:34:46 +0100

    - fix address validation - add transfer adventurer tx function - begin impl of optimistically blanking transferred adventurers in list

--- ui/src/app/components/start/AdventurerListCard.tsx
+++ ui/src/app/components/start/AdventurerListCard.tsx
@@ -6,26 +6,44 @@ import { Contract, validateChecksumAddress } from "starknet";
 import useTransactionCartStore from "@/app/hooks/useTransactionCartStore";
 import { useAccount } from "@starknet-react/core";
 import useAdventurerStore from "@/app/hooks/useAdventurerStore";
+import { padAddress } from "@/app/lib/utils";
+import { AccountInterface } from "starknet";
 
 export interface AdventurerListCardProps {
   adventurer: Adventurer;
   gameContract: Contract;
   handleSwitchAdventurer: (adventurerId: number) => Promise<void>;
+  transferAdventurer: (
+    account: AccountInterface,
+    adventurerId: number,
+    from: string,
+    recipient: string
+  ) => Promise<void>;
 }
 
 export const AdventurerListCard = ({
   adventurer,
   gameContract,
   handleSwitchAdventurer,
+  transferAdventurer,
 }: AdventurerListCardProps) => {
-  const { address } = useAccount();
+  const { account, address } = useAccount();
   const [isTransferOpen, setIsTransferOpen] = useState(false);
   const [transferAddress, setTransferAddress] = useState("");
 
   const setAdventurer = useAdventurerStore((state) => state.setAdventurer);
 
-  const validAddress =
-    validateChecksumAddress(transferAddress) && transferAddress;
+  const validAddress = (() => {
+    const paddedAddress = padAddress(transferAddress);
+    if (paddedAddress.length !== 66 || !transferAddress.startsWith("0x")) {
+      return false;
+    }
+    try {
+      return validateChecksumAddress(paddedAddress);
+    } catch {
+      return false;
+    }
+  })();
 
   const addToCalls = useTransactionCartStore((state) => state.addToCalls);
 
@@ -44,56 +62,74 @@ export const AdventurerListCard = ({
         <div className="absolute bottom-0 flex flex-row bg-terminal-black items-center justify-center w-full z-[2]">
           <Button
             size={"lg"}
-            variant={"token"}
+            variant={"default"}
             onClick={() => {
               setAdventurer(adventurer);
               handleSwitchAdventurer(adventurer.id!);
             }}
             className="w-1/2"
           >
-            Select
+            Play
           </Button>
           <Button
             size={"lg"}
-            variant={isTransferOpen ? "default" : "token"}
+            variant={"token"}
             onClick={() => setIsTransferOpen(!isTransferOpen)}
-            className="w-1/2"
+            className={`w-1/2 ${isTransferOpen && "animate-pulse"}`}
           >
             Transfer
           </Button>
           {isTransferOpen && (
-            <div className="absolute bottom-20 bg-terminal-black border border-terminal-green flex flex-col gap-2 items-center justify-center w-3/4 p-2">
-              <span className="uppercase text-2xl">Enter Address</span>
-              <div className="flex flex-col w-full items-center justify-center gap-10">
-                <input
-                  type="text"
-                  value={transferAddress}
-                  onChange={(e) => {
-                    let value = e.target.value.toLowerCase();
-                    if (!value.startsWith("0x")) {
-                      value = "0x" + value.replace(/[^0-9a-f]/g, "");
-                    } else {
-                      value = "0x" + value.slice(2).replace(/[^0-9a-f]/g, "");
-                    }
-                    setTransferAddress(value);
-                  }}
-                  className="p-1 h-12 text-2xl w-3/4 bg-terminal-black border border-terminal-green animate-pulse transform uppercase"
-                />
-                {transferAddress && !validAddress && (
-                  <p className="absolute bottom-15 text-terminal-yellow">
-                    INVALID ADDRESS!
-                  </p>
-                )}
-                <Button
-                  size={"lg"}
-                  onClick={() => handleAddTransferTx(transferAddress)}
-                >
-                  Send
-                </Button>
+            <>
+              <div className="absolute bottom-20 bg-terminal-black border border-terminal-green flex flex-col gap-2 items-center justify-center w-3/4 p-2">
+                <span className="uppercase text-2xl">Enter Address</span>
+                <div className="flex flex-col w-full items-center justify-center gap-10">
+                  <input
+                    type="text"
+                    value={transferAddress}
+                    onChange={(e) => {
+                      let value = e.target.value;
+                      setTransferAddress(value);
+                    }}
+                    className="p-1 h-12 text-2xl w-3/4 bg-terminal-black border border-terminal-green animate-pulse transform uppercase"
+                  />
+                  {transferAddress && !validAddress && (
+                    <p className="absolute bottom-15 text-terminal-yellow">
+                      INVALID ADDRESS!
+                    </p>
+                  )}
+                  <div className="flex flex-row gap-2 items-center">
+                    <Button
+                      size={"lg"}
+                      onClick={() =>
+                        transferAdventurer(
+                          account!,
+                          adventurer?.id!,
+                          address!,
+                          transferAddress
+                        )
+                      }
+                      disabled={!validAddress}
+                    >
+                      Send
+                    </Button>
+                    <Button
+                      size={"lg"}
+                      variant={"ghost"}
+                      onClick={() => handleAddTransferTx(transferAddress)}
+                      disabled={!validAddress}
+                    >
+                      Add to Cart
+                    </Button>
+                  </div>
+                </div>
               </div>
-            </div>
+            </>
           )}
         </div>
+      )}
+      {isTransferOpen && (
+        <div className="absolute inset-0 bg-terminal-black/75 z-[1]" />
       )}
       <Info adventurer={adventurer} gameContract={gameContract} />
     </>
--- ui/src/app/components/start/AdventurersList.tsx
+++ ui/src/app/components/start/AdventurersList.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, useCallback, useMemo } from "react";
-import { Contract } from "starknet";
+import { Contract, AccountInterface, validateChecksumAddress } from "starknet";
 import { Button } from "@/app/components/buttons/Button";
 import useAdventurerStore from "@/app/hooks/useAdventurerStore";
 import { CoinIcon, HeartIcon, SkullIcon } from "@/app/components/icons/Icons";
@@ -12,6 +12,7 @@ import useNetworkAccount from "@/app/hooks/useNetworkAccount";
 import { indexAddress, padAddress, calculateLevel } from "@/app/lib/utils";
 import { Adventurer } from "@/app/types";
 import { AdventurerListCard } from "@/app/components/start/AdventurerListCard";
+import useTransactionCartStore from "@/app/hooks/useTransactionCartStore";
 
 export interface AdventurerListProps {
   isActive: boolean;
@@ -20,6 +21,12 @@ export interface AdventurerListProps {
   gameContract: Contract;
   adventurersCount: number;
   aliveAdventurersCount: number;
+  transferAdventurer: (
+    account: AccountInterface,
+    adventurerId: number,
+    from: string,
+    recipient: string
+  ) => Promise<void>;
 }
 
 export const AdventurersList = ({
@@ -29,12 +36,14 @@ export const AdventurersList = ({
   gameContract,
   adventurersCount,
   aliveAdventurersCount,
+  transferAdventurer,
 }: AdventurerListProps) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const [showZeroHealth, setShowZeroHealth] = useState(true);
+  const [isTransferOpen, setIsTransferOpen] = useState(false);
   const buttonRefs = useRef<(HTMLButtonElement | null)[]>([]);
   const network = useUIStore((state) => state.network);
-  const { account } = useNetworkAccount();
+  const { account, address } = useNetworkAccount();
   const owner = account?.address ? padAddress(account.address) : "";
   const adventurersPerPage = 10;
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -43,6 +52,31 @@ export const AdventurersList = ({
   const { refetch, setData, isLoading } = useQueriesStore();
 
   const setAdventurer = useAdventurerStore((state) => state.setAdventurer);
+
+  const [transferAddress, setTransferAddress] = useState("");
+
+  const validAddress = (() => {
+    const paddedAddress = padAddress(transferAddress);
+    if (paddedAddress.length !== 66 || !transferAddress.startsWith("0x")) {
+      return false;
+    }
+    try {
+      return validateChecksumAddress(paddedAddress);
+    } catch {
+      return false;
+    }
+  })();
+
+  const addToCalls = useTransactionCartStore((state) => state.addToCalls);
+
+  const handleAddTransferTx = (recipient: string, adventurerId: number) => {
+    const transferTx = {
+      contractAddress: gameContract?.address ?? "",
+      entrypoint: "transfer_from",
+      calldata: [address!, recipient, adventurerId.toString() ?? "", "0"],
+    };
+    addToCalls(transferTx);
+  };
 
   const adventurersVariables = useMemo(() => {
     return {
@@ -128,7 +162,7 @@ export const AdventurersList = ({
     <div className="flex flex-col items-center h-full">
       {formatAdventurersCount > 0 ? (
         <div className="flex flex-col gap-2 sm:flex-row sm:gap-5 w-full h-full items-center sm:items-start">
-          <div className="flex flex-col items-center sm:w-1/2 border border-terminal-green h-full">
+          <div className="flex flex-col items-center sm:w-1/2 border border-terminal-green h-full w-full">
             <span className="relative flex items-center justify-center bg-terminal-green-50 w-full">
               <h2 className="text-xl uppercase text-center text-terminal-black h-10 flex items-center justify-center m-0">
                 Adventurers
@@ -144,46 +178,127 @@ export const AdventurersList = ({
                 </Button>
               )}
             </span>
-            <div className="relative flex flex-col w-full overflow-y-auto default-scroll mx-2 sm:mx-0 border border-terminal-green sm:border-none h-[350px] xl:h-[500px] 2xl:h-[625px]">
+            <div className="relative flex flex-col w-full overflow-y-auto default-scroll mx-2 sm:mx-0 border border-terminal-green sm:border-none h-[625px] 2xl:h-[625px]">
               <div className="h-7/8 flex flex-col  w-full overflow-y-auto default-scrol">
-                {adventurers.map((adventurer, index) => (
-                  <Button
-                    key={index}
-                    ref={(ref) => (buttonRefs.current[index] = ref)}
-                    className={`text-lg sm:text-base
-                    ${
-                      selectedIndex === index && isActive ? "animate-pulse" : ""
-                    }`}
-                    variant={selectedIndex === index ? "default" : "ghost"}
-                    size={"lg"}
-                    onClick={async () => {
-                      setSelectedIndex(index);
-                      await handleSelectAdventurer(adventurer.id!);
-                    }}
-                    disabled={adventurer?.health === 0}
-                  >
-                    <div className="aboslute w-full inset-0 flex flex-row justify-between">
-                      <p>{`${adventurer.name} - ${adventurer.id}`}</p>
-                      <div className="flex flex-row items-center gap-2">
-                        <span className="flex flex-row gap-1">
-                          <p>LVL</p>
-                          <p>{calculateLevel(adventurer.xp!)}</p>
-                        </span>
-                        <div className="flex flex-row gap-1">
-                          <HeartIcon className="w-5 fill-current" />
-                          <span>{adventurer.health}</span>
-                        </div>
-                        <div className="flex flex-row text-terminal-yellow gap-1">
-                          <CoinIcon className="w-5 fill-current" />
-                          <span>{adventurer.gold}</span>
-                        </div>
-                      </div>
-                      {adventurer?.health === 0 && (
-                        <SkullIcon className="w-3 fill-current" />
+                {!isTransferOpen ? (
+                  adventurers.map((adventurer, index) => (
+                    <div className="relative w-full" key={index}>
+                      {index === selectedIndex && (
+                        <>
+                          <div className="sm:hidden absolute inset-0 bg-terminal-black/75" />
+                          <div className="sm:hidden absolute flex flex-row gap-5 w-full h-full items-center justify-center">
+                            <Button
+                              size={"lg"}
+                              onClick={() => {
+                                setAdventurer(adventurer);
+                                handleSwitchAdventurer(adventurer.id!);
+                              }}
+                            >
+                              Play
+                            </Button>
+                            <Button
+                              size={"lg"}
+                              variant={"contrast"}
+                              className="border border-terminal-green"
+                              onClick={() => setIsTransferOpen(true)}
+                            >
+                              Transfer
+                            </Button>
+                          </div>
+                        </>
                       )}
+                      <Button
+                        key={index}
+                        ref={(ref) => (buttonRefs.current[index] = ref)}
+                        className={`text-lg sm:text-base w-full hover:animate-none`}
+                        variant={selectedIndex === index ? "default" : "ghost"}
+                        size={"lg"}
+                        onClick={async () => {
+                          setSelectedIndex(index);
+                          await handleSelectAdventurer(adventurer.id!);
+                        }}
+                        disabled={adventurer?.health === 0}
+                      >
+                        <div className="aboslute w-full inset-0 flex flex-row justify-between">
+                          <p className="w-1/2 overflow-hidden whitespace-nowrap text-ellipsis text-left">{`${adventurer.name} - ${adventurer.id}`}</p>
+                          <div className="flex flex-row items-center gap-2">
+                            <span className="flex flex-row gap-1">
+                              <p>LVL</p>
+                              <p>{calculateLevel(adventurer.xp!)}</p>
+                            </span>
+                            <div className="flex flex-row gap-1">
+                              <HeartIcon className="w-5 fill-current" />
+                              <span>{adventurer.health}</span>
+                            </div>
+                            <div className="flex flex-row text-terminal-yellow gap-1">
+                              <CoinIcon className="w-5 fill-current" />
+                              <span>{adventurer.gold}</span>
+                            </div>
+                          </div>
+                          {adventurer?.health === 0 && (
+                            <SkullIcon className="w-3 fill-current" />
+                          )}
+                        </div>
+                      </Button>
                     </div>
-                  </Button>
-                ))}
+                  ))
+                ) : (
+                  <div className="sm:hidden flex flex-col bg-terminal-black gap-2 items-center justify-center w-full h-full p-2">
+                    <Button
+                      size="lg"
+                      variant={"token"}
+                      onClick={() => setIsTransferOpen(false)}
+                    >
+                      Back
+                    </Button>
+                    <span className="uppercase text-2xl">Enter Address</span>
+                    <div className="flex flex-col w-full items-center justify-center gap-10">
+                      <input
+                        type="text"
+                        value={transferAddress}
+                        onChange={(e) => {
+                          let value = e.target.value;
+                          setTransferAddress(value);
+                        }}
+                        className="p-1 h-12 text-2xl w-3/4 bg-terminal-black border border-terminal-green animate-pulse transform uppercase"
+                      />
+                      {transferAddress && !validAddress && (
+                        <p className="absolute bottom-15 text-terminal-yellow">
+                          INVALID ADDRESS!
+                        </p>
+                      )}
+                      <div className="flex flex-row gap-2 items-center">
+                        <Button
+                          size={"lg"}
+                          onClick={() =>
+                            transferAdventurer(
+                              account!,
+                              adventurers[selectedIndex].id!,
+                              address!,
+                              transferAddress
+                            )
+                          }
+                          disabled={!validAddress}
+                        >
+                          Send
+                        </Button>
+                        <Button
+                          size={"lg"}
+                          variant={"ghost"}
+                          onClick={() =>
+                            handleAddTransferTx(
+                              transferAddress,
+                              adventurers[selectedIndex].id!
+                            )
+                          }
+                          disabled={!validAddress}
+                        >
+                          Add to Cart
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
               {formatAdventurersCount > 10 && (
                 <div className="absolute bottom-0 flex items-end justify-center w-full h-1/8">
@@ -225,6 +340,7 @@ export const AdventurersList = ({
                   adventurer={adventurers[selectedIndex]}
                   gameContract={gameContract}
                   handleSwitchAdventurer={handleSwitchAdventurer}
+                  transferAdventurer={transferAdventurer}
                 />
               )}
             </div>
