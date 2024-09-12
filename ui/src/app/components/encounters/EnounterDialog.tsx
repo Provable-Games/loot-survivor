1a86f591d4154afaa626c4b3c3b6ce9d9e826fd7
Starknet Dev <starknetdev@gmail.com>
2024-09-12 14:08:37 -0400

    improve styles of prescience draggable

--- ui/src/app/components/encounters/EnounterDialog.tsx
+++ ui/src/app/components/encounters/EnounterDialog.tsx
@@ -5,17 +5,18 @@ import EncounterTable from "./EncounterTable";
 
 const EncounterDialog = () => {
   const showEncounterTable = useUIStore((state) => state.showEncounterTable);
-
   return (
-    <Draggable>
-      <div className="flex flex-col gap-5 sm:gap-0 sm:flex-row justify-between w-full bg-terminal-black max-h-[300px] border border-terminal-green text-xs sm:text-base sm:overflow-y-scroll default-scroll">
-        <MdClose
-          className="w-10 h-10 absolute top-0 right-0 z-10 border-y border-l border-bottom border-terminal-green bg-terminal-black cursor-pointer"
-          onClick={() => showEncounterTable(false)}
-        />
-        <EncounterTable />
-      </div>
-    </Draggable>
+    <div className="fixed z-50">
+      <Draggable>
+        <div className="flex flex-col gap-5 sm:gap-0 sm:flex-row justify-between w-full bg-terminal-black max-h-[300px] border border-terminal-green text-xs sm:text-base sm:overflow-y-scroll default-scroll">
+          <MdClose
+            className="w-10 h-10 absolute top-0 right-0 z-10 border-y border-l border-bottom border-terminal-green bg-terminal-black cursor-pointer"
+            onClick={() => showEncounterTable(false)}
+          />
+          <EncounterTable />
+        </div>
+      </Draggable>
+    </div>
   );
 };
 
