472d15478eecaa91ce1c5f4409c4111d224f307a
broody <broody@gmail.com>
2025-01-15 11:27:32 -1000

    fix controller connector

--- ui/src/app/provider.tsx
+++ ui/src/app/provider.tsx
@@ -1,15 +1,87 @@
 "use client";
-import { controllerConnector } from "@/app/lib/connectors";
+
+import ControllerConnector from "@cartridge/connector/controller";
 import { Chain, mainnet, sepolia } from "@starknet-react/chains";
 import {
-  jsonRpcProvider,
+  Connector,
   StarknetConfig,
+  jsonRpcProvider,
   starkscan,
   useInjectedConnectors,
 } from "@starknet-react/core";
 import React from "react";
+import { constants } from "starknet";
 import { Network } from "./hooks/useUIStore";
 import { networkConfig } from "./lib/networkConfig";
+
+const controllerConnector = new ControllerConnector({
+  policies: {
+    contracts: {
+      ["0x12"]: {
+        methods: [
+          {
+            name: "New Game",
+            entrypoint: "new_game",
+            description: "Starts a new Loot Survivor game.",
+          },
+          {
+            name: "Explore",
+            entrypoint: "explore",
+            description: "Explore the dungeon.",
+          },
+          {
+            name: "Attack",
+            entrypoint: "attack",
+            description: "Attack the beast.",
+          },
+          {
+            name: "Flee",
+            entrypoint: "flee",
+            description: "Flee the beast.",
+          },
+          {
+            name: "Equip",
+            entrypoint: "equip",
+            description: "Equip a LOOT item.",
+          },
+          {
+            name: "Drop",
+            entrypoint: "drop",
+            description: "Drop a LOOT item.",
+          },
+          {
+            name: "Upgrade",
+            entrypoint: "upgrade",
+            description: "Upgrade Adventurer.",
+          },
+          {
+            name: "Transfer",
+            entrypoint: "transfer_from",
+            description: "Transfer an Adventurer.",
+          },
+        ],
+      },
+      ["0x23"]: {
+        methods: [
+          {
+            name: "Enter Tournament",
+            entrypoint: "enter_tournament",
+            description: "Enter a tournament.",
+          },
+          {
+            name: "Start Tournament",
+            entrypoint: "start_tournament",
+            description: "Start a tournament.",
+          },
+        ],
+      },
+    },
+  },
+  chains: [{ rpcUrl: "https://api.cartridge.gg/x/starknet/mainnet" }],
+  defaultChainId: constants.StarknetChainId.SN_MAIN,
+  theme: "loot-survivor",
+  colorMode: "dark",
+}) as never as Connector;
 
 export function StarknetProvider({
   children,
@@ -37,12 +109,7 @@ export function StarknetProvider({
       chains={[network === "mainnet" ? mainnet : sepolia]}
       connectors={[
         ...connectors,
-        controllerConnector(
-          networkConfig[network!].gameAddress,
-          networkConfig[network!].tournamentAddress,
-          networkConfig[network!].lordsAddress,
-          networkConfig[network!].rpcUrl
-        ),
+        controllerConnector,
       ]}
       explorer={starkscan}
       provider={jsonRpcProvider({ rpc })}
